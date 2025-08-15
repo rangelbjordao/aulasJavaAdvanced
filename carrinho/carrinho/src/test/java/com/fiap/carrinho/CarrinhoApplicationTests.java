f00d41ab22b563977f5076e863fec9902926b1f1
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2025-08-15 08:41:32 -0300

    criacao projeto de carrinho

---
+++ carrinho/carrinho/src/test/java/com/fiap/carrinho/CarrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.carrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
