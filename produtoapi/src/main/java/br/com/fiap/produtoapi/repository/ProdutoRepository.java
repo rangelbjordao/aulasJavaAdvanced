19bfe6f1b5c974bea44497c1db7116cab2ddbbb9
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2025-09-17 18:21:30 -0300

    ProdutoRepository criado

--- produtoapi/src/main/java/br/com/fiap/produtoapi/repository/ProdutoRepository.java
+++ produtoapi/src/main/java/br/com/fiap/produtoapi/repository/ProdutoRepository.java
@@ -1,4 +1,8 @@
 package br.com.fiap.produtoapi.repository;
 
-public class ProdutoRepository {
+import br.com.fiap.produtoapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
 }
