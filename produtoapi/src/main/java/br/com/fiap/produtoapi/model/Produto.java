e04261a92144a57d75961fd493ed3f978132308f
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2025-09-17 18:19:24 -0300

    Classe produto criada

---
+++ produtoapi/src/main/java/br/com/fiap/produtoapi/model/Produto.java
@@ -0,0 +1,29 @@
+package br.com.fiap.produtoapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "tb_produto")
+public class Produto {
+    @Id
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 50)
+    private String nome;
+
+    @Column(name = "preco", nullable = false)
+    private BigDecimal preco;
+
+    @Column(name = "data_cadastro", nullable = false)
+    private LocalDateTime dataCadastro;
+
+    @PrePersist
+    public void prePersist() {
+        this.dataCadastro = LocalDateTime.now();
+    }
+}
