02b3a4203081779a6685ac4e89f223dbd8172830
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-02-23 12:02:07 -0300

    Cors configurado

--- movies/src/main/java/br/com/fiap/movies/Movie.java
+++ movies/src/main/java/br/com/fiap/movies/Movie.java
@@ -17,6 +17,7 @@ public class Movie {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private String synopsis;
     private BigDecimal rating;
     private String sinopse;
     private String posterUrl;
