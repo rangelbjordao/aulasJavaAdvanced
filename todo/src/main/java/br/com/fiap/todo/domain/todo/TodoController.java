9dfdbce515d9fa61469f96487343c1c3d70db079
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-03-09 12:00:54 -0300

    Base Backend

---
+++ todo/src/main/java/br/com/fiap/todo/domain/todo/TodoController.java
@@ -0,0 +1,27 @@
+package br.com.fiap.todo.domain.todo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @PostMapping
+    public
+
+}
---
+++ todo/src/main/java/br/com/fiap/todo/domain/todo/TodoRepository.java
@@ -0,0 +1,6 @@
+package br.com.fiap.todo.domain.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
