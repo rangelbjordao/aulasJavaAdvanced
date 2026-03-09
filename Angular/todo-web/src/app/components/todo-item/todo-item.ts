d5f551bc3aca67fe6589f62cf04451cd818f243a
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-03-09 12:00:29 -0300

    projeto To Do web

---
+++ Angular/todo-web/src/app/components/todo-item/todo-item.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  imports: [],
+  templateUrl: './todo-item.html',
+  styleUrl: './todo-item.css',
+})
+export class TodoItem {
+  id = input<number>();
+  description = input<string>();
+  completed = input<boolean>();
+}
---
+++ Angular/todo-web/src/app/service/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Todo {
+  id: number;
+  description: string;
+  completed: boolean;
+}
+
+@Injectable({ providedIn: 'root' })
+export class ApiService {
+  private http = inject(HttpClient);
+  private baseUrl = 'http://localhost:8080/todos';
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.baseUrl);
+  }
+}
